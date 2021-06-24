ae7d4ab23cb8c027dc76d7691a91b3a41e7e7663
Thunderator <vinicios1010@hotmail.com>
2021-06-24 12:48:17 -0300

    Criado AcessoriesDiv para armazenar o conteúdo dos acessórios e centralizar; Criado AcessoriesSpanDiv para armazenar o span com o título; Criado ButtonItem com as propriedades do botão principal dos acessórios;

---
+++ components/Accessories/AccessoriesStyles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const AccessoriesDiv = styled.div`
+  display: flex;
+  width: 100%;
+  height: auto;
+  align-items: center;
+`
+export const AccessoriesSpanDiv = styled.div`
+  margin-bottom: 0.5rem;
+  font-size: 1.4rem;
+  letter-spacing: 0.2rem;
+`
+export const ButtonItem = styled.button<{
+  borderColor: string
+  backgroundColor: string
+  gradientColor: string
+}>`
+  margin-bottom: 1rem;
+  border-radius: 0.5rem;
+  border-width: 0.3rem;
+
+  background-image: ${({ backgroundColor, gradientColor }) =>
+    `linear-gradient(to top left, ${backgroundColor} 0%, ${gradientColor} 100%)`};
+  background-image: ${({ backgroundColor, gradientColor }) =>
+    `linear-gradient(top left, ${backgroundColor} 0%, ${gradientColor} 100%)`};
+
+  border-color: ${({ borderColor }) => borderColor};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0.5rem;
+  width: 5.4rem;
+  height: 5.4rem;
+`
